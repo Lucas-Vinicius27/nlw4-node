0d3cf1d60abcc52807ddb9d58f46f2595ba4fd87
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-02-26 01:17:36 -0300

    Update UserController.ts

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -6,19 +6,13 @@ class UserController {
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
         const usersRepository = getCustomRepository(UsersRepository);
-        const userAlreadyExists = await usersRepository.findOne({
-            email
-        });
+        const userAlreadyExists = await usersRepository.findOne({ email });
 
         if (userAlreadyExists) {
-            return response.status(400).json({
-                error: "User Already Exists!"
-            });
+            return response.status(400).json({ error: "User already exists!" });
         }
 
-        const user = usersRepository.create({
-            name, email
-        });
+        const user = usersRepository.create({ name, email });
 
         await usersRepository.save(user);
 
